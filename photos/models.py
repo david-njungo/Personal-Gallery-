ef684c1a36746a449910ba24c5873b625e674422
David Njungo <davidnjungo3@gmail.com>
2021-09-06 21:43:00 +0300

    create image model

--- photos/models.py
+++ photos/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     # article_image = models.ImageField(upload_to = '')
-    image_name = models.CharField(max_length =30)
+    name = models.CharField(max_length =30)
     description = models.CharField(max_length =30)
+    location = models.ForeignKey('Location',on_delete=models.CASCADE)
+    link = models.CharField(max_length = 150)   
+
+    def __str__(self):
+        return self.name
+
 
