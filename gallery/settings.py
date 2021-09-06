05196c258f8849c670b5cc39e2acc154d046f66d
David Njungo <davidnjungo3@gmail.com>
2021-09-06 09:38:51 +0300

    create base.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos.apps.PhotosConfig',
+    'photos',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
