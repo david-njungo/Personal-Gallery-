1bb53524c724889c26f29e00d394286f4b77af7e
David Njungo <davidnjungo3@gmail.com>
2021-09-06 22:08:06 +0300

    create category testclass

--- photos/models.py
+++ photos/models.py
@@ -5,7 +5,7 @@ class Image(models.Model):
     # article_image = models.ImageField(upload_to = '')
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =30)
-    category = models.CharField(max_length =30)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
     location = models.ForeignKey('Location',on_delete=models.CASCADE)
     link = models.CharField(max_length = 150)   
 
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Image,Category,Location
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.category = Category(name = 'food')
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
